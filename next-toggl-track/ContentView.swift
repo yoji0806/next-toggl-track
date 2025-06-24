a4f8e395c6750cedaf04d2970c9a2902bd42992b
Yoji Yamamoto <ajfrlc9808@gmail.com>
2025-06-24 20:12:41 +0900

    マウスクリックの取得

--- next-toggl-track/ContentView.swift
+++ next-toggl-track/ContentView.swift
@@ -80,74 +80,85 @@ class KeyboardMonitor: NSObject {
     func startMonitoring() {
         NSEvent.addGlobalMonitorForEvents(matching: [.keyDown, .leftMouseDown, .rightMouseDown], handler: { (event: NSEvent) in
     
-            var pressedKey = String()
-            
-            switch event.modifierFlags.rawValue {
-                case 256:   //character or Enter or Space
-                    switch event.keyCode {
-                    case 36: pressedKey = "↵"   //Enter
-                    case 48: pressedKey = "⇥"
-                    case 49: pressedKey = "␣"   //Space
-                    case 51: pressedKey = "⌫"   //Delete
-                    case 53: pressedKey = "⎋"    //Escape(esc)
-                    case 102: pressedKey = "【英数】"   //英数
-                    case 104: pressedKey = "【かな】"   //かな
-                    default: pressedKey = event.characters ?? ""  //normal key
-                    }
-                case 65792:     //Shift(locked) + character
-                    switch event.keyCode {
-                    case 36: pressedKey = "↵"   //Enter
-                    case 48: pressedKey = "⇥"
-                    case 49: pressedKey = "␣"   //Space
-                    case 51: pressedKey = "⌫"   //Delete
-                    case 53: pressedKey = "⎋"    //Escape(esc)
-                    case 102: pressedKey = "【英数】"   //英数
-                    case 104: pressedKey = "【かな】"   //かな
-                    default: pressedKey = event.characters ?? ""  //normal key
-                    }
-                    //TODO: 他に、以下のすべてのキー入力のFlag番号が変わるが、あまり入力されないだろうから、一旦そのまま。
-                case 131330: pressedKey = "\(event.characters ?? "")"  //Shift + character
-                case 262401: pressedKey = "【Command】\(event.characters ?? "")" //control + character
-                case 524576: pressedKey = "\(event.characters ?? "")"    //Option + character
-                case 1048840: pressedKey = "⌘\(event.characters ?? "")"  //Command(L) + character
-                case 1048848: pressedKey = "⌘\(event.characters ?? "")"  //Command(R) + character
-                case 1179914: pressedKey = "⌘\(event.characters ?? "")"   //Command(L) + Shift + character
-                case 1179922: pressedKey = "⌘\(event.characters ?? "")"   //Command(R) + Shift + character
-                case 1573160: pressedKey = "⌘\(event.characters ?? "")"   //Command(L) + option + character
-                case 1573168: pressedKey = "⌘\(event.characters ?? "")"   //Command(R) + option + character
-                case 8388864:   //fn
-                    switch event.specialKey?.rawValue {
-                    case nil: pressedKey = "🌐\(event.characters ?? "")"  //fn + character
-                    case 63236: pressedKey = "【F1】"
-                    case 63237: pressedKey = "【F2】"
-                    case 63238: pressedKey = "【F3】"
-                    case 63239: pressedKey = "【F4】"
-                    case 63240: pressedKey = "【F5】"
-                    case 63241: pressedKey = "【F6】"
-                    case 63242: pressedKey = "【F7】"
-                    case 63243: pressedKey = "【F8】"
-                    case 63244: pressedKey = "【F9】"
-                    case 63245: pressedKey = "【F10】"
-                    case 63246: pressedKey = "【F11】"
-                    case 63247: pressedKey = "【F12】"
-                    default: pressedKey = "【unknown fn】"
-                    }
-                case 10486016:  //arrow
-                    switch event.keyCode {
-                    case 123: pressedKey = "←"
-                    case 124: pressedKey = "→"
-                    case 125: pressedKey = "↓"
-                    case 126: pressedKey = "↑"
-                    default: pressedKey = event.characters ?? ""
-                    }
-     
-                default: pressedKey = "【unknown Flags char:\(event.characters ?? "") keycode:\(event.keyCode)】 specialKey:\(event.specialKey?.rawValue)"
+            var action = String()
+
+            if event.type == .leftMouseDown {
+                action = "【l_click】"
+                print("Click: left")
+            } else if event.type == .rightMouseDown {
+                action = "【r_click】"
+                print("Click: right")
+            } else if event.type == .keyDown {
+
+                switch event.modifierFlags.rawValue {
+                    case 256:   //character or Enter or Space
+                        switch event.keyCode {
+                        case 36: action = "↵"   //Enter
+                        case 48: action = "⇥"
+                        case 49: action = "␣"   //Space
+                        case 51: action = "⌫"   //Delete
+                        case 53: action = "⎋"    //Escape(esc)
+                        case 102: action = "【英数】"   //英数
+                        case 104: action = "【かな】"   //かな
+                        default: action = event.characters ?? ""  //normal key
+                        }
+                    case 65792:     //Shift(locked) + character
+                        switch event.keyCode {
+                        case 36: action = "↵"   //Enter
+                        case 48: action = "⇥"
+                        case 49: action = "␣"   //Space
+                        case 51: action = "⌫"   //Delete
+                        case 53: action = "⎋"    //Escape(esc)
+                        case 102: action = "【英数】"   //英数
+                        case 104: action = "【かな】"   //かな
+                        default: action = event.characters ?? ""  //normal key
+                        }
+                        //TODO: 他に、以下のすべてのキー入力のFlag番号が変わるが、あまり入力されないだろうから、一旦そのまま。
+                    case 131330: action = "\(event.characters ?? "")"  //Shift + character
+                    case 262401: action = "【Command】\(event.characters ?? "")" //control + character
+                    case 524576: action = "\(event.characters ?? "")"    //Option + character
+                    case 1048840: action = "⌘\(event.characters ?? "")"  //Command(L) + character
+                    case 1048848: action = "⌘\(event.characters ?? "")"  //Command(R) + character
+                    case 1179914: action = "⌘\(event.characters ?? "")"   //Command(L) + Shift + character
+                    case 1179922: action = "⌘\(event.characters ?? "")"   //Command(R) + Shift + character
+                    case 1573160: action = "⌘\(event.characters ?? "")"   //Command(L) + option + character
+                    case 1573168: action = "⌘\(event.characters ?? "")"   //Command(R) + option + character
+                    case 8388864:   //fn
+                        switch event.specialKey?.rawValue {
+                        case nil: action = "🌐\(event.characters ?? "")"  //fn + character
+                        case 63236: action = "【F1】"
+                        case 63237: action = "【F2】"
+                        case 63238: action = "【F3】"
+                        case 63239: action = "【F4】"
+                        case 63240: action = "【F5】"
+                        case 63241: action = "【F6】"
+                        case 63242: action = "【F7】"
+                        case 63243: action = "【F8】"
+                        case 63244: action = "【F9】"
+                        case 63245: action = "【F10】"
+                        case 63246: action = "【F11】"
+                        case 63247: action = "【F12】"
+                        default: action = "【unknown fn】"
+                        }
+                    case 10486016:  //arrow
+                        switch event.keyCode {
+                        case 123: action = "←"
+                        case 124: action = "→"
+                        case 125: action = "↓"
+                        case 126: action = "↑"
+                        default: action = event.characters ?? ""
+                        }
+        
+                    default: action = "【unknown Flags char:\(event.characters ?? "") keycode:\(event.keyCode)】 specialKey:\(event.specialKey?.rawValue)"
+                }
+                print("Input: \(event.characters)  KeyCode:\(event.keyCode)   Flag:\(event.modifierFlags.rawValue) SpecialKey:\(event.specialKey?.rawValue)")
+
+            } else {
+                action = "unknown"
             }
-    
             
-            print("Input: \(event.characters)  KeyCode:\(event.keyCode)   Flag:\(event.modifierFlags.rawValue) SpecialKey:\(event.specialKey?.rawValue)")
             DispatchQueue.main.async {
-                self.textInput.data += pressedKey
+                self.textInput.data += action
             }
         
         })
